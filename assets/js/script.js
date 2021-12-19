9f75456390aaa8bd08d8dc2ef2352359d72158d8
zachary-levin <60529010+zachary-levin@users.noreply.github.com>
2021-12-19 01:13:26 -0600

    updated to display password sample when confirming on initial prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 // Assignment code here
 
+function generatePassword() {
+  console.log("You clicked the button.");
+  var promptUser = window.confirm('Would you like to generate a password?');
+
+// 1. Prompt the user for the password criteria
+//    a. Password length 8 < 128 characters
+//    b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input. 
+// 3. Generate password based on criteria.
+
+//Math.random().toString(36).slice(2);
+
+// 4. Display password in the page.
+  return "Generated password will go here.";
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
